83d54f80f02e0c91be5fa07e8253e39a9776aeeb
Danil <danilbystrov8@gmail.com>
2025-06-05 14:52:34 +0300

    Optimization Components

--- src/GameLayout/Information/Information.jsx
+++ src/GameLayout/Information/Information.jsx
@@ -1,19 +1,11 @@
 import { InformationLayout } from "./InformationLayout"
 import { PLAYER } from "../../constants/player"
 import { STATUS } from "../../constants/status"
+import { ACTION } from "../../constants/action"
+import { PLAYER_NAME } from "../../constants/player-name"
 import PropTypes from 'prop-types';
 
 export const Information = ({ status, currentPlayer, winner }) => {
-    const ACTION = {
-        [STATUS.TURN]: 'Ходит',
-        [STATUS.WIN]: 'Победил',
-		[STATUS.DRAW]: 'Ничья',
-    }
-    const PLAYER_NAME = {
-        [PLAYER.X]: 'Крестик',
-        [PLAYER.O]: 'Нолик',
-    }
-
     let information;
 
     if (status === STATUS.WIN) {
